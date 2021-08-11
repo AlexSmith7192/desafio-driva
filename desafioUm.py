15c4069cbbbf31689f8993f0fdecc54ea4ffe6e1
AlexSmith7192 <alexsmith7192@gmail.com>
2021-08-11 19:22:24 -0300

    print filtro pelo capital social do arquivo DadosEmpresa.csv

--- desafioUm.py
+++ desafioUm.py
@@ -13,3 +13,6 @@ print('\nQuantidade de empresas que optam pelo simples: \n', optionsSimplemSum)
 
 sumShareCapital = de.sum().iloc[[5]]
 print('\nSoma do capital social das empresas: \n', sumShareCapital)
+
+filterCapitalSocial = de.query('capital_social > 10000 & capital_social < 20000')
+print('\nEmpresas que tem capital social maior que 10.000 e menor que 20.000: \n', filterCapitalSocial)
