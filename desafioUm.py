e0eb2c189955c8c6e92ce10fcd629cbe59b9c37f
AlexSmith7192 <alexsmith7192@gmail.com>
2021-08-11 19:29:46 -0300

    faz leitura do arquivo DadosEndereco.csv

--- desafioUm.py
+++ desafioUm.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
-de = pd.read_csv('DadosEmpresa.csv')
+dEmp = pd.read_csv('DadosEmpresa.csv')
 
-columnsDe = de.columns
+columnsDe = dEmp.columns
 print('\nColunas da tabela DadosEmpresa.csv: \n', columnsDe)
 
-firstLines = de.head(10)
+firstLines = dEmp.head(10)
 print('\n10 primeiras linhas da tabela DadosEmpresa.csv: \n', firstLines)
 
-optionsSimplemSum = de.query('opcao_pelo_simples == "SIM"').count().iloc[[4]]
+optionsSimplemSum = dEmp.query('opcao_pelo_simples == "SIM"').count().iloc[[4]]
 print('\nQuantidade de empresas que optam pelo simples: \n', optionsSimplemSum)
 
-sumShareCapital = de.sum().iloc[[5]]
+sumShareCapital = dEmp.sum().iloc[[5]]
 print('\nSoma do capital social das empresas: \n', sumShareCapital)
 
-filterCapitalSocial = de.query('capital_social > 10000 & capital_social < 20000')
+filterCapitalSocial = dEmp.query('capital_social > 10000 & capital_social < 20000')
 print('\nEmpresas que tem capital social maior que 10.000 e menor que 20.000: \n', filterCapitalSocial)
+
+dEnd = pd.read_csv('DadosEndereco.csv')
