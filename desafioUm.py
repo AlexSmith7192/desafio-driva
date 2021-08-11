459bd512506f7adfd673df651cc737d1f9de4a27
AlexSmith7192 <alexsmith7192@gmail.com>
2021-08-11 17:28:32 -0300

    print das 10 primeiras linhas do arquivo DadosEmpresa.csv

--- desafioUm.py
+++ desafioUm.py
@@ -3,5 +3,7 @@ import pandas as pd
 de = pd.read_csv('DadosEmpresa.csv')
 
 columnsDe = de.columns
+print('\nColunas da tabela DadosEmpresa.csv: \n', columnsDe)
 
-print('Colunas da tabela DadosEmpresa.csv: ', columnsDe)
+firstLines = de.head(10)
+print('\n10 primeiras linhas da tabela DadosEmpresa.csv: \n', firstLines)
